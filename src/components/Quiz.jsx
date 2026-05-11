23218486f4de9e2d241e9f04e1e53076838765b4
korvmacka <shawnabirnbaum@email.arizona.edu>
2026-05-11 15:26:13 +0200

    Updated API URL

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -13,7 +13,7 @@ function Quiz() {
 
   async function fetchQuestions() {
     const response = await fetch(
-      "https://opentdb.com/api.php?amount=10&type=multiple"
+      "https://opentdb.com/api.php?amount=10"
     );
 
     const data = await response.json();
